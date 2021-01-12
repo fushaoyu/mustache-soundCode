3fa2e503c3c0aa91920c84aa70ebb8ec8b765324
fushaoyu <fushaoyu@qianxin.com>
2021-01-12 15:15:09 +0800

    备注说明

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import renderTemplate from "./renderTemplate"
 //提供全局的方法
 window.SetTemplate = {
     rander(template, data) {
-
-        var mm = {
-            a: {
-                b: {
-                    c: 100
-                }
-            }
-        }
         //调用parseTemplateToTokens方法，让模板字符串能够变为tokens数组
         var tokens = parseTemplateToTokens(template);
         //调用renderTemplate函数，让tokens数组变为dom字符串
---
+++ 介绍.md
@@ -0,0 +1,3 @@
+### 了解mustache
+* 手写parseTemplateToTokens方法让模板字符串能够变为tokens数组
+* 手写renderTemplate函数，让tokens数组变为dom字符串
